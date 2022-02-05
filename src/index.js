becf72705a285ef5a32d7d89afefd1631547d814
lovetech95 <leminhnghia111095@gmail.com>
2022-02-05 13:15:39 +0700

    new commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 import './index.css';
 import App from './App';
 
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-            <Router basename="/practice2sell" >
+            <Router history={history} basename="/practice2sell" >
                 <App/>
             </Router>,
             document.getElementById('root')
